2d9e18cd52ca441dab9c6dc8a803486ae0edbe06
Laksh Gangwani (Student) <lgangwan@sundevils.asu.edu>
2023-06-04 17:31:26 -0700

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 const App = () => {
   return (
-    <>
-    <h1>Welcome to Laksh</h1>
-    </>
+    <Router>
+      
+        <Header />
+        <main className='py-2'><Container>
+          <Routes>
+          <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart/:id?' element={<CartScreen />} />
+          </Routes>
+            
+
+          
+            </Container></main>
+        <Footer />
+    </Router>
+    
   );
 }
 
